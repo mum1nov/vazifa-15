d72fab81896e80e6b5db76d1939c459bb85c9fb1
muminovaziz <muminov.me.2001@gmail.com>
2022-10-23 03:20:27 +0500

    add java script and css folder create html code

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var elbutton = document.querySelector(".header__span")
+var elBody =document.querySelector("body")
+
+
+
+elbutton.addEventListener("click", function(){ elBody.classList.toggle("select")})
